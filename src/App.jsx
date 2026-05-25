8fdcb52b867ce103ab77395e0de1b4ac47a38ad3
Vinxkumar <vinxkumar178@gmail.com>
2026-05-25 12:50:28 +0530

    changed backend url

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 
-const API = "http://localhost:5000/api";
+const API = "https://autoattendance-be.onrender.com/api";
 
 const FONTS = `@import url('https://fonts.googleapis.com/css2?family=Syne:wght@400;600;700;800&family=DM+Sans:ital,wght@0,300;0,400;0,500;1,300&display=swap');`;
 
